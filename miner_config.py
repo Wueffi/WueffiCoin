88f86b4dad4c2477dae86420a49ed06628267c0c
Wueffi <101346296+Wueffi@users.noreply.github.com>
2022-03-15 22:13:48 +0100

    Add files via upload

---
+++ miner_config.py
@@ -0,0 +1,11 @@
+"""Konfiguriere diese Details bevor du den Miner.py startest.
+"""
+
+# Trage deine generierte Walletadresse ein:
+MINER_ADDRESS = "FirstWallet"
+
+# Trage deine node url oder ip ein. Wenn du das nicht hast benutze "http://localhost:5000".
+MINER_NODE_URL = "http://localhost:5000"
+
+#Nicht bearbeiten
+PEER_NODES = []
